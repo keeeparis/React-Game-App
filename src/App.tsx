22d9f3b05edd38681c042fbfefa5b09634d5028f
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-15 15:35:16 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import { MouseEvent, useRef, useState } from 'react'
+import './App.scss'
+import Game from './game/Game'
+
+function App() {
+    const circleRef = useRef<HTMLDivElement>(null)
+    const redRef = useRef<HTMLDivElement>(null)
+    const blueRef = useRef<HTMLDivElement>(null)
+    const yellowRef = useRef<HTMLDivElement>(null)
+    const greenRef = useRef<HTMLDivElement>(null)
+    const roundRef = useRef<HTMLDivElement>(null)
+    const loseRef = useRef<HTMLDivElement>(null)
+    const audioRef = useRef<HTMLAudioElement>(null)
+
+    const handleStartGame = () => {
+        new Game(
+            circleRef.current,
+            redRef.current, 
+            blueRef.current, 
+            yellowRef.current, 
+            greenRef.current,
+            roundRef.current,
+            loseRef.current,
+            audioRef.current
+        ).init()
+    }
+
+    const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.add('active')
+    }
+
+    const handleMouseUp = (e: MouseEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.remove('active')
+    }
+
+    return (
+        <div className="App">
+            <div className='Circle' ref={circleRef}>
+                <div 
+                    className='Red'
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}  
+                    ref={redRef}
+                    data-tile={1}
+                    />
+                <div 
+                    className='Blue'
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}  
+                    ref={blueRef}
+                    data-tile={2}
+                />
+                <div 
+                    className='Yellow'
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}  
+                    ref={yellowRef}
+                    data-tile={3}
+                />
+                <div 
+                    className='Green'
+                    onMouseDown={handleMouseDown}
+                    onMouseUp={handleMouseUp}  
+                    ref={greenRef}
+                    data-tile={4}
+                />
+            </div>
+            <div className='Data'>
+                <div>
+                    <h1 ref={roundRef}>Round: 0</h1>
+                    <button onClick={handleStartGame}>Start</button>
+                </div>
+                <div className='Options'>
+                    <h2>Game Options</h2>
+                    <div className='Option'>
+                        <input type="radio" name="game" id="normal" />
+                        <label htmlFor="normal">Normal</label>
+                    </div>
+                    <div className='Option'>
+                        <input type="radio" name="game" id="sound-only" />
+                        <label htmlFor="sound-only">Sound Only</label>
+                    </div>
+                    <div className='Option'>
+                        <input type="radio" name="game" id="light-only" />
+                        <label htmlFor="light-only">Light Only</label>
+                    </div>
+                </div>
+                <div className='Lose' ref={loseRef} />
+            </div>
+            <audio className='Audio' autoPlay ref={audioRef}></audio>
+        </div>
+    )
+}
+
+export default App
---
+++ src/game/Game.ts
@@ -0,0 +1,143 @@
+class Game {
+    constructor(
+        circle: any, 
+        red: any, 
+        blue: any, 
+        yellow: any, 
+        green: any, 
+        round: any,
+        lose: any,
+        audio: any
+    ) {
+        this.circle = circle
+        this.red = red
+        this.blue = blue
+        this.yellow = yellow
+        this.green = green
+        this.roundDiv = round
+        this.loseDiv = lose
+        this.audioDiv = audio
+    }
+    
+    circle: HTMLDivElement
+    red: HTMLDivElement
+    blue: HTMLDivElement
+    yellow: HTMLDivElement
+    green: HTMLDivElement
+    roundDiv: HTMLDivElement
+    loseDiv: HTMLDivElement
+    audioDiv: HTMLAudioElement
+
+    round: number = 0
+    active: boolean = true
+    sequence: number[] = []
+    copySequence: number[] = []
+    tileArray: HTMLDivElement[] = []
+
+    init() {
+        this.loseDiv.classList.remove('visible')
+        this.startGame()
+    }
+    
+    startGame() {
+        this.tileArray = [this.red, this.blue, this.yellow, this.green]
+        this.round = 0
+        this.sequence = []
+        this.copySequence = []
+        this.newRound()
+    }
+
+    newRound() {
+        this.round += 1
+        this.roundDiv.innerText = `Round: ${this.round}`
+        this.sequence.push(this.randomNumber())
+        this.copySequence = this.sequence.slice(0)
+        this.animate(this.sequence)
+    }
+
+    activateBoard() {
+        this.circle.classList.remove('deactivated')
+        this.tileArray.forEach(tile => {
+            tile.onclick = this.registerClick(tile.dataset.tile)
+        })
+    }
+
+    deactivateBoard() {
+        this.circle.classList.add('deactivated')
+    }
+
+    registerClick(e: any) {
+        return () => {
+            const trueResponse = this.copySequence.shift()
+            const actualResponse = +e
+            this.active = (trueResponse === actualResponse)
+            this.checkLose()
+        }
+    }
+
+    checkLose() {
+        if (this.copySequence.length === 0 && this.active) {
+            this.deactivateBoard()
+            this.newRound()
+        } else if (!this.active) {
+            this.deactivateBoard()
+            this.endGame() 
+        }
+    }
+
+    endGame() {
+        this.round = 0
+        this.roundDiv.innerText = `Round: ${this.round}`
+        this.loseDiv.innerText = `
+            Wow! Your result is ${this.round}. 
+            Don't Stop and Keep Playing!
+        `
+        this.loseDiv.classList.add('visible')
+    }
+
+    /* HELPER FUNCTIONS */
+
+    randomNumber() {
+        return Math.floor((Math.random() * 4) + 1)
+    }
+
+    animate(sequence: number[]) {
+        let i = 0
+        let interval = setInterval(() => {
+            this.lightUp(sequence[i])
+            this.playSound(sequence[i])
+
+            i++
+            if (i >= sequence.length) {
+                clearInterval(interval)
+                this.activateBoard()
+            }
+        }, 600)
+    }
+
+    lightUp(tile: number) {
+        this.tileArray.forEach(element => {
+            if (element.dataset.tile && tile === +element.dataset.tile) {
+                element.classList.add('active')
+                setTimeout(() => {
+                    element.classList.remove('active')
+                }, 250)
+            }
+        })
+    }
+
+    playSound(tile: number) {
+        this.audioDiv.innerHTML = `
+            <source src='src/sounds/${tile}.ogg' type='audio/ogg' />
+            <source src='src/sounds/${tile}.mp3' type='audio/mp3' />
+        `
+    }
+
+    /* GETTERS */
+
+    get getRound() {
+        return this.round
+    }
+}
+
+export default Game
